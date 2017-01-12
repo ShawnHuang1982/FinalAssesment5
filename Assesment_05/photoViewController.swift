a34ec3489ce44ae7b543d8f39691b89bb482295f
shawn <com.appshawn.HelloGoogleMap2A>
2017-01-13 01:10:38 +0800

    新增功能:1.可以拍照存檔,2.可以致中放大縮小照片,3.鍵盤可以升上時畫面往上提

---
+++ Assesment_05/photoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  photoViewController.swift
+//  Assesment_05
+//
+//  Created by  shawn on 11/01/2017.
+//  Copyright © 2017 shawn. All rights reserved.
+//
+
+import UIKit
+
+class photoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
